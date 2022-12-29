87bc2c33af43ec54b5c76dbb1802086d30d5334e
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-12-29 13:31:26 +1000

    A configuration file and two files (lib.rs and main.rs) were changed. The configuration file sets the encoding to UTF-8, indent style to tabs, end of line to LF, and trims trailing whitespace. For files with the .md extension, trailing whitespace is not trimmed. Strings with the .yml extension use two spaces for indentation. The modified lib.rs file adds a serialize and deserialize trait to a struct and modifies a function to make an API call with a header and request body, and to return a deserialized response. The main.rs file was modified to check for staged git changes and, if there are none, to print the git status. If there are staged changes, a prompt is created with the changes and an API call is made to get commit messages, which are then presented to the user. The user can select a commit message or edit it, and then the commit is executed using git.

--- src/lib.rs
+++ src/lib.rs
@@ -4,55 +4,52 @@ use std::process::Command;
 
 #[derive(Serialize)]
 struct GenerateTextRequest<'a> {
-    model: &'a str,
-    prompt: &'a str,
-    temperature: f32,
-    max_tokens: usize,
-    n: usize,
+	model: &'a str,
+	prompt: &'a str,
+	temperature: f32,
+	max_tokens: usize,
+	n: usize,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Choice {
-    pub text: String,
+	pub text: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct GenerateTextResponse {
-    pub choices: Vec<Choice>,
+	pub choices: Vec<Choice>,
 }
 
 pub async fn get_commit_messages<'a>(
-    client: &Client,
-    api_key: &str,
-    model: &'a str,
-    prompt: &'a str,
-    temperature: f32,
-    max_tokens: usize,
-    n: usize,
+	client: &Client,
+	api_key: &str,
+	model: &'a str,
+	prompt: &'a str,
+	temperature: f32,
+	max_tokens: usize,
+	n: usize,
 ) -> Result<GenerateTextResponse, Error> {
-    let request_body = GenerateTextRequest {
-        model,
-        prompt,
-        temperature,
-        max_tokens,
-        n,
-    };
+	let request_body = GenerateTextRequest {
+		model,
+		prompt,
+		temperature,
+		max_tokens,
+		n,
+	};
 
-    client
-        .post("https://api.openai.com/v1/completions")
-        .header("Authorization", format!("Bearer {}", api_key))
-        .json(&request_body)
-        .send()
-        .await?
-        .json()
-        .await
+	client
+		.post("https://api.openai.com/v1/completions")
+		.header("Authorization", format!("Bearer {}", api_key))
+		.json(&request_body)
+		.send()
+		.await?
+		.json()
+		.await
 }
 
 pub fn spawn_cmd(cmd: &str, args: &[String]) -> String {
-    let output = Command::new(cmd)
-        .args(args)
-        .output()
-        .expect("Failed to execute process");
+	let output = Command::new(cmd).args(args).output().expect("Failed to execute process");
 
-    String::from_utf8(output.stdout).expect("Not UTF-8")
+	String::from_utf8(output.stdout).expect("")
 }
--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,58 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let diff = spawn_cmd("git", &["diff".to_string(), "--staged".to_string()]);
+	let diff = spawn_cmd("git", &["diff".to_string(), "--staged".to_string()]);
 
-    if diff.len() == 0 {
-        let output = spawn_cmd(
-            "git",
-            &[
-                "-c".to_string(),
-                "color.status=always".to_string(),
-                "status".to_string(),
-            ],
-        );
-        println!("{}", output);
-        std::process::exit(exitcode::NOINPUT);
-    }
+	if diff.len() == 0 {
+		let output = spawn_cmd(
+			"git",
+			&[
+				"-c".to_string(),
+				"color.status=always".to_string(),
+				"status".to_string(),
+			],
+		);
+		println!("{}", output);
+		std::process::exit(exitcode::NOINPUT);
+	}
 
-    let api_key = "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";
-    let args = args().collect::<Vec<String>>();
-    let mut git_args: Vec<String> = vec!["git".to_string(), "commit".to_string()]; // TODO: remove git
+	let api_key = "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";
+	let args = args().collect::<Vec<String>>();
+	let mut git_args: Vec<String> = vec!["git".to_string(), "commit".to_string()]; // TODO: remove git
 
-    if !args.contains(&String::from("-m")) && !args.contains(&String::from("--message")) {
-        let prompt = format!("This is a diff from a recent change of a library. Explain what was changed as concise as possible.\n{}", diff);
-        let client = reqwest::Client::new();
-        let answer =
-            get_commit_messages(&client, api_key, "text-davinci-003", &prompt, 0.8, 200, 3).await?;
+	if !args.contains(&String::from("-m")) && !args.contains(&String::from("--message")) {
+		let prompt = format!(
+			"This is a diff from a recent change of a library. Explain what was changed as concise as possible.\n{}",
+			diff
+		);
+		let client = reqwest::Client::new();
+		let answer = get_commit_messages(&client, api_key, "text-davinci-003", &prompt, 0.8, 200, 3).await?;
 
-        let items: Vec<String> = answer
-            .choices
-            .iter()
-            .map(|choice| choice.text.trim().to_string())
-            .collect();
-        let selection = Select::with_theme(&ColorfulTheme::default())
-            .with_prompt("Which commit message do you prefer?")
-            .items(&items)
-            .default(0)
-            .report(false)
-            .clear(true)
-            .interact()?;
+		let items: Vec<String> = answer.choices.iter().map(|choice| choice.text.trim().to_string()).collect();
+		let selection = Select::with_theme(&ColorfulTheme::default())
+			.with_prompt("Which commit message do you prefer?")
+			.items(&items)
+			.default(0)
+			.report(false)
+			.clear(true)
+			.interact()?;
 
-        if let Some(msg) = Editor::new()
-            .require_save(false)
-            .extension("txt")
-            .edit(&items[selection].replace('"', "\\\""))
-            .unwrap()
-        {
-            git_args.push("--message".into());
-            git_args.push(format!("\"{}\"", msg));
-        } else {
-            std::process::exit(exitcode::NOINPUT);
-        }
-    }
+		if let Some(msg) =
+			Editor::new().require_save(false).extension("txt").edit(&items[selection].replace('"', "\\\"")).unwrap()
+		{
+			git_args.push("--message".into());
+			git_args.push(format!("\"{}\"", msg));
+		} else {
+			std::process::exit(exitcode::NOINPUT);
+		}
+	}
 
-    // passing through all flags to git commit
-    for item in args.iter().skip(1) {
-        git_args.push(item.to_string());
-    }
-    let output = spawn_cmd("echo", &git_args); // TODO: replace echo with git
+	// passing through all flags to git commit
+	for item in args.iter().skip(1) {
+		git_args.push(item.to_string());
+	}
+	let output = spawn_cmd("echo", &git_args); // TODO: replace echo with git
 
-    println!("\n{}", output);
-    Ok(())
+	println!("\n{}", output);
+	Ok(())
 }
