10fa95998d6f08fe0e87e897d8816ae8175dbb2d
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-12-30 10:50:25 +1000

    "Update spinner to dots instead of dots9"

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			println!("The diff is too large for this tool.\nStage less files or commit with \"git commit\" directly.");
 			std::process::exit(exitcode::USAGE);
 		}
-		let mut sp = Spinner::new(Spinners::Dots9, "Asking GPT3 to write the commit message".into());
+		let mut sp = Spinner::new(Spinners::Dots, "Asking GPT3 to write the commit message".into());
 		let client = reqwest::Client::new();
 		let answer = get_commit_messages(&client, &api_key, "text-davinci-003", &prompt, 0.8, 200, 3).await?;
 		sp.stop();
+		print!("\x1b[2K\n\x1b[2F\n");
 
 		let items: Vec<String> = answer
 			.choices
